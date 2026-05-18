5c20d8a9c6462996387ac5fc301e76d48a1f06a7
Srijita Ghosh <srijitaghoshxia@gmail.com>
2026-05-19 03:31:41 +0530

    Strengthen backend CORS headers for deployed frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,6 +50,12 @@ app.use(
   })
 );
 app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use("/uploads", express.static(UPLOADS_DIR));
 
